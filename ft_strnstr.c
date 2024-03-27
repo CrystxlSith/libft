d11e3c9b995122719a4f4d4c78103516f29c0c61
Wbelyne <joaquim1810@gmail.com>
2024-03-27 21:32:57 +0100

    strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crystal <crystal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 21:31:37 by crystal           #+#    #+#             */
+/*   Updated: 2024/03/27 21:32:46 by crystal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*strnstr - Find the first substring in a length-limited string*/
+
+char	*ft_strnstr(const char * s1, const char * s2, size_t len)
+{
+	
+}
+
+int main(void)
+{
+	
+	return 0;
+}
