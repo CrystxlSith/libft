32738912f2f3b2bfbd813c46458b77718ac94591
Wbelyne <joaquim1810@gmail.com>
2024-03-28 13:56:39 +0100

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: crystal <crystal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 21:31:37 by crystal           #+#    #+#             */
-/*   Updated: 2024/03/27 21:32:46 by crystal          ###   ########.fr       */
+/*   Updated: 2024/03/28 13:55:55 by crystal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,29 @@
 
 /*strnstr - Find the first substring in a length-limited string*/
 
-char	*ft_strnstr(const char * s1, const char * s2, size_t len)
+char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
-	
-}
+	size_t	i;
+	size_t	j;
 
-int main(void)
-{
-	
-	return 0;
+	i = 0;
+	if (len == 0 || s1 == s2)
+		return (s1);
+	while (s2[i] && len)
+	{
+		j = 0;
+		if (s2[i] == s1[j])
+		{
+			while (s1[j] && s2[i + j] == s1[j] && len)
+			{
+				len--;
+				j++;
+			}
+		}
+		if (s1[j] == '\0')
+			return ((char *)s2 + i);
+		i++;
+		len--;
+	}
+	return (NULL);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: crystal <crystal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 11:36:35 by wbelyne           #+#    #+#             */
-/*   Updated: 2024/03/27 19:47:59 by crystal          ###   ########.fr       */
+/*   Updated: 2024/03/28 13:54:54 by crystal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	**ft_split(char const *s, char c);
 char	*ft_strtrim(char const *s1, char const *set);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+char	*ft_strnstr(const char *s1, const char *s2, size_t len);
 
 #endif
