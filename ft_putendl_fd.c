ff7927d35127944da841ac8aa3ea6f2fc18f9836
Wbelyne <joaquim1810@gmail.com>
2024-04-11 14:11:47 +0200

    putendl

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,13 +6,13 @@
 /*   By: crystal <crystal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 21:32:45 by crystal           #+#    #+#             */
-/*   Updated: 2024/04/09 21:50:05 by crystal          ###   ########.fr       */
+/*   Updated: 2024/04/11 14:11:31 by crystal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
 	int	i;
 
@@ -21,15 +21,7 @@ void ft_putendl_fd(char *s, int fd)
 	{
 		while (s[i])
 			write(fd, &s[i++], 1);
-		if (s[i] == '\0') 
+		if (s[i] == '\0')
 			write(fd, "\n", 1);
 	}
-}
-
-int main(void)
-{
-	int i = open("text.txt", O_WRONLY);
-	char *str = "manger dans moin cul lulu";
-	ft_putendl_fd(str, i);
-	return 0;
 }
