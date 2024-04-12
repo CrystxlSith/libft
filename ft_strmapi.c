8cc97ed2331d8bf87f04c59e351a5da1a37553fc
Wbelyne <joaquim1810@gmail.com>
2024-04-12 17:53:58 +0200

    strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -3,20 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wbelyne <wbelyne@student.42.fr>            +#+  +:+       +#+        */
+/*   By: crystal <crystal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 14:20:18 by wbelyne           #+#    #+#             */
-/*   Updated: 2024/04/12 14:23:09 by wbelyne          ###   ########.fr       */
+/*   Updated: 2024/04/12 17:53:54 by crystal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
-
-int main(void)
 {
-	char *str = "je syis la";
-	aff(str, &ft_toupper);
-	return 0;
+	char	*newstr;
+	int		i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	newstr = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!newstr)
+		return (NULL);
+	while (s[i])
+	{
+		newstr[i] = f(i, s[i]);
+		i++;
+	}
+	newstr[i] = '\0';
+	return (newstr);
 }
