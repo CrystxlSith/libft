17f5ea533717adf2e963823381def41f4b6ea0ed
Joaquim Pfeiffer <wbelyne@MacBook-Air-de-Joaquim.local>
2024-04-19 10:11:27 +0200

    lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -3,14 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crystal <crystal@student.42.fr>            +#+  +:+       +#+        */
+/*   By: wbelyne <wbelyne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 18:34:46 by crystal           #+#    #+#             */
-/*   Updated: 2024/04/18 18:35:52 by crystal          ###   ########.fr       */
+/*   Updated: 2024/04/19 09:56:19 by wbelyne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	delete(void *lst)
+{
+	free(lst->content);
+}
 
 void ft_lstdelone(t_list *lst, void (*del)(void *))
 {
@@ -19,6 +24,7 @@ void ft_lstdelone(t_list *lst, void (*del)(void *))
 
 int main(int argc, char const *argv[])
 {
-	
+	t_list *list = ft_lstnew("manger");
+	ft-ft_lstdelone(list, &delete);
 	return 0;
 }
