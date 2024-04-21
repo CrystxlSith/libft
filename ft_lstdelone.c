fa3162382a1fc3bd0fead92bc1a8c9bd479476cc
Joaquim Pfeiffer <wbelyne@MacBook-Air-de-Joaquim.local>
2024-04-21 11:09:04 +0200

    lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crystal <crystal@student.42.fr>            +#+  +:+       +#+        */
+/*   By: wbelyne <wbelyne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 18:34:46 by crystal           #+#    #+#             */
-/*   Updated: 2024/04/19 15:29:13 by crystal          ###   ########.fr       */
+/*   Updated: 2024/04/21 11:08:47 by wbelyne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,22 @@ void	delete(void *content)
 	free(content);
 }
 
-void ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	// if (!lst || !del)
-	// 	return ;
-	// (del)(lst->content);
-	// free(lst);
-	// lst = NULL;
 	if (!lst || !del)
 		return ;
 	del(lst->content);
 	free(lst);
 }
 
-int main(void)
-{
-	// t_list *list = ft_lstnew("manger");
-	// printf("%s", (char *)list->content);
-	// ft_lstdelone(list, delete);
-	t_list *elem = ft_lstnew("Hello");
-	ft_lstdelone(elem, delete);
-	return 0;
-}
+// int main(void)
+// {
+// 	// t_list *list = ft_lstnew("manger");
+// 	// printf("%s", (char *)list->content);
+// 	// ft_lstdelone(list, delete);
+// 	char *content = (char *)malloc(sizeof(char) * 6);
+// 	sprintf(content, "Hello");
+// 	t_list *elem = ft_lstnew(content);
+// 	ft_lstdelone(elem, &delete);
+// 	return 0;
+// }
