e58130d957b7bd53f94f7ed46f424e08b7385b0a
Wbelyne <joaquim1810@gmail.com>
2024-04-26 13:17:02 +0200

    lstiter

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -3,25 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wbelyne <wbelyne@student.42.fr>            +#+  +:+       +#+        */
+/*   By: crystal <crystal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 21:00:00 by crystal           #+#    #+#             */
-/*   Updated: 2024/04/25 13:40:30 by wbelyne          ###   ########.fr       */
+/*   Updated: 2024/04/26 13:16:14 by crystal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <ctype.h>
-
-void	ft_alph(void *content)
-{
-	char *str = (char *)content;
-	while (*str)
-	{
-		*str = toupper(*str);
-		str++;
-	}
-}
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
@@ -34,10 +24,12 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 	}
 }
 
-int main(void)
-{
-	t_list *head = ft_lstnew("hello");
-	ft_lstadd_back(&head, ft_lstnew("world"));
-	ft_lstiter(head, &ft_alph);
-	return 0;
-}
+// int main(void)
+// {
+// 	char *mall = (char *)malloc(sizeof(char) * 6);
+// 	sprintf(mall, "hello");
+// 	t_list *head = ft_lstnew(mall);
+// 	ft_lstadd_back(&head, ft_lstnew("world"));
+// 	ft_lstiter(head, &ft_alph);
+// 	return 0;
+// }
