202546a9789f13158d435ac7b30862b884144476
CrystalSith <joaquim1810@gmail.com>
2024-05-17 16:36:36 +0200

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jopfeiff <jopfeiff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 19:30:23 by crystal           #+#    #+#             */
-/*   Updated: 2024/05/15 14:28:35 by jopfeiff         ###   ########.fr       */
+/*   Updated: 2024/05/17 16:35:01 by jopfeiff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,35 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dlen;
+	size_t	slen;
 	size_t	i;
+	int	j;
 
-	dlen = 0;
-	while (dst[dlen] && dlen < size)
-		dlen++;
+	dlen = ft_strlen(dst);
+	slen = ft_strlen(src);
+	j = 0;
 	i = dlen;
-	while (src[dlen - i] && dlen < size - 1)
+	if (size == 0)
+		return (slen);
+	if (size < dlen)
+		return (slen + size);
+	while (src[j] && i < size - 1)
 	{
-		dst[dlen] = src[dlen - i];
-		dlen++;
+		dst[i] = src[j];
+		j++;
+		i++;
 	}
 	if (dlen < size)
 		dst[dlen + i] = '\0';
-	return (dlen + ft_strlen(src));
+	return (dlen + slen);
 }
 
 // int main() {
-//     char dst[SIZE] = "Hello, ";
+//     char dst[20] = "Hello, ";
 //     char src[] = "world!";
 //     size_t dst_len = strlen(dst);
 
-//     size_t result = ft_strlcat(dst, src, SIZE);
+//     size_t result = ft_strlcat(dst, src, 0);
 
 //     printf("Chaîne concaténée : %s\n", dst);
 //     printf("Longueur totale : %zu\n", result);
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jopfeiff <jopfeiff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 20:31:43 by crystal           #+#    #+#             */
-/*   Updated: 2024/05/15 13:51:15 by jopfeiff         ###   ########.fr       */
+/*   Updated: 2024/05/17 16:06:56 by jopfeiff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*str;
 	size_t	i;
 	size_t	j;
-
+	
 	j = 0;
 	i = start;
-	str = (char *)malloc(sizeof(char) * (len + 1));
+	str = (char *)malloc(sizeof(char) * len + 1);
 	if (!str)
 		return (NULL);
-	while (s[i] && j < len)
+	while (s[i])
 	{
+		if (i >= start && j)
 		str[j] = s[i];
 		j++;
 		i++;
