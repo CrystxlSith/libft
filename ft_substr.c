b5b2b0e5ed24fb70ce60697f7e681a744abab4a9
CrystalSith <joaquim1810@gmail.com>
2024-05-20 11:41:19 +0200

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jopfeiff <jopfeiff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 20:31:43 by crystal           #+#    #+#             */
-/*   Updated: 2024/05/17 16:06:56 by jopfeiff         ###   ########.fr       */
+/*   Updated: 2024/05/20 11:41:10 by jopfeiff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,28 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	
 	j = 0;
 	i = start;
-	str = (char *)malloc(sizeof(char) * len + 1);
+	while (s[i] && j < len)
+	{
+		j++;
+		i++;
+	}
+	str = (char *)malloc(sizeof(char) * j + 1);
 	if (!str)
 		return (NULL);
-	while (s[i])
+	j = 0;
+	i = start;
+	while (s[i] && j < len)
 	{
-		if (i >= start && j)
 		str[j] = s[i];
 		j++;
 		i++;
 	}
 	str[j] = '\0';
 	return (str);
+}
+int main(void)
+{
+	char *s = ft_substr("tripouille", 100, 1);
+	printf("%s", s);
+	return 0;
 }
